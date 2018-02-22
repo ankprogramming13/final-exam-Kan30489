1a068c008a0e3c1281ade5cb1b08e3cd4f7995bb
Unknown <Kan_30489@hotmail.com>
2018-02-22 11:27:15 +0700

    กาญรักพ่อแม่ กาญรักโรงเรียน กาญรักอาจารย์

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
+    int cmd;
+        /* code */
+        ofstream writefile ("std.txt"), ofstream::app);
+        ifstream readFile ("std.txt");
+        string line;
+        string name;
+
+        cin >> cmd;
+        if (cmd== 1)(
+            cin >>name;
+            if (writefile.is_open()) {
+                writefile << name << endl;
+                writefile.close ();
+            }else {
+                cout << "Unable to open file";}
 
         return 0;
 }
